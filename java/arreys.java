e34c9131ab9598fb35a52e1ae2131b25cdfb8d77
satrohan shah <satrohanshah59@gmail.com>
2024-05-20 16:05:47 +0530

    java

---
+++ java/arreys.java
@@ -0,0 +1,35 @@
+public class arreys {
+    public static void main(String[] args) {
+        //there are three diffrent method of arreys
+        //1. declearation + memory allocation
+                
+                   /*
+                 int[] marks= new int[5];
+                    marks[0]=40;
+                    marks[1]=59;
+                    marks[2]=96;
+                    marks[3]=16;
+                    marks[4]=33;
+
+                    */
+        //2.declearation, memory allocation
+                 /*
+                    int[] marks1;
+                    marks1=new int[5];
+                    marks1[0]=40;
+                    marks1[1]=59;
+                    marks1[2]=96;
+                    marks1[3]=16;
+                    marks1[4]=33;
+                  */
+        //3.declearation+initialize
+        /*   
+        int[] marks2={40,30,50,69,50};
+          */
+
+           //print the result!
+           
+           //System.out.println(marks[2]);
+
+    }
+}
