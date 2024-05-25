638b3852b9a83f2b491c6ae2aafc5ae333f5af09
satrohan shah <satrohanshah59@gmail.com>
2024-05-25 19:55:13 +0530

    java

--- java/varargs.java
+++ java/varargs.java
@@ -1,7 +1,14 @@
 public class varargs {
-    public static void sum(int arr){
-        int a[];
-        
+    public static int sum(int ...arr){
+        int result=0;
+        for(int a: arr){
+            result += a;
+        }
+        return result;
 
+    }
+    public static void main(String[] args) {
+        System.out.println("sum of 4 and 5 is :" + sum(4,5));
+        System.out.println("sum of 1 and 59 is :" + sum(1,59));
     }
 }
